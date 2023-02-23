b6c1097986f7042204b84b5a1ab56285c63d05ec
ratul <ratulsikder104@gmail.com>
2023-02-23 15:13:01 +0600

    Project init

---
+++ app/Controllers/BannerController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controllers;
+
+
+class BannerController  {
+
+    function index() {
+        echo "Welcome";
+    }
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+
+$config= new App\Controllers\BannerController();
+$config->index();
+
+?>
